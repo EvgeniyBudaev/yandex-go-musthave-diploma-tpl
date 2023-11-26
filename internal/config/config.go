f7477d0c95b561fb809b09ef417cd5085a1f66b3
Evgeniy Budaev <budaev.e@gmail.com>
2023-11-26 08:10:07 +0300

    config

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,13 +2,12 @@ package config
 
 import (
 	"flag"
-	"github.com/joho/godotenv"
-	"github.com/kelseyhightower/envconfig"
 	"log"
+	"os"
 )
 
 type Config struct {
-	ServerAddr     string `env:"SERVER_ADDRESS"`
+	ServerAddr     string `env:"RUN_ADDRESS"`
 	DBURI          string `env:"DATABASE_URI"`
 	AccrualSysAddr string `env:"ACCRUAL_SYSTEM_ADDRESS"`
 }
@@ -16,23 +15,28 @@ type Config struct {
 var config Config
 
 func Init() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("error while Init. Error in Load")
-		return
-	}
-	err := envconfig.Process("MYAPP", &config)
-	if err != nil {
-		log.Fatal("error while Init. Error in Process")
-		return
-	}
-
 	flag.StringVar(&config.ServerAddr, "a", "", "GopherMart server address")
 	flag.StringVar(&config.DBURI, "d", "", "GopherMart database address")
 	flag.StringVar(&config.AccrualSysAddr, "r", "", "Accrual system address")
 	flag.Parse()
 
-	log.Printf("Got ServerAddr %s, DBURI %s, AccrualSysAddr %s to run GopherMart",
-		&config.ServerAddr, &config.DBURI, &config.AccrualSysAddr)
+	ServerAddrEnv := os.Getenv("RUN_ADDRESS")
+	if ServerAddrEnv != "" {
+		config.ServerAddr = ServerAddrEnv
+	}
+
+	DBURIEnv := os.Getenv("DATABASE_URI")
+	if DBURIEnv != "" {
+		config.DBURI = DBURIEnv
+	}
+
+	AccrualSysAddrEnv := os.Getenv("ACCRUAL_SYSTEM_ADDRESS")
+	if AccrualSysAddrEnv != "" {
+		config.AccrualSysAddr = AccrualSysAddrEnv
+	}
+
+	log.Printf("Got ServerAddr %s, DBURI %s, AccrualSysAddr %s to run GopherMart", &config.ServerAddr,
+		&config.DBURI, &config.AccrualSysAddr)
 }
 
 func GetServerAddr() string {

f24398369efba3864b3809dbe522e14a939fae8c
Evgeniy Budaev <budaev.e@gmail.com>
2023-12-04 15:22:19 +0300

    http.StatusOK

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -244,8 +244,8 @@ func (strg *HandlerWithStorage) AddOrder(w http.ResponseWriter, r *http.Request)
 		return
 	}
 	userID := r.Context().Value(UserID).(string)
-	statusCode, founded, err := strg.storage.AddOrderForUser(r.Context(), string(data), userID)
-	statusCode = http.StatusOK
+	_, founded, err := strg.storage.AddOrderForUser(r.Context(), string(data), userID)
+	statusCode := http.StatusOK
 
 	if founded && err != nil {
 		statusCode = http.StatusConflict

b7d08e5f00d7b7f430f73c246b8f84bd5ab11f49
Evgeniy Budaev <budaev.e@gmail.com>
2023-12-04 16:40:43 +0300

    revert

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -244,27 +244,18 @@ func (strg *HandlerWithStorage) AddOrder(w http.ResponseWriter, r *http.Request)
 		return
 	}
 	userID := r.Context().Value(UserID).(string)
-	_, founded, err := strg.storage.AddOrderForUser(r.Context(), string(data), userID)
-	if !founded && err != nil {
-		log.Printf("error add order into db, %d", http.StatusInternalServerError)
-		http.Error(w, "error add order into db", http.StatusInternalServerError)
+	statusCode, err := strg.storage.AddOrderForUser(r.Context(), string(data), userID)
+	if err != nil {
+		log.Printf("error add order into db, %d", statusCode)
+		http.Error(w, "error add order into db", statusCode)
 		return
 	}
-	if founded && err == nil {
-		log.Printf("error add order into db, %d", http.StatusOK)
-		http.Error(w, "error add order into db", http.StatusOK)
-		return
+	if statusCode == http.StatusAccepted {
+		go func(orderNumber string) {
+			strg.ordersToProcess <- orderNumber
+		}(string(data))
 	}
-	if founded && err != nil {
-		log.Printf("error add order into db, %d", http.StatusConflict)
-		http.Error(w, "error add order into db", http.StatusConflict)
-		return
-	}
-
-	go func(orderNumber string) {
-		strg.ordersToProcess <- orderNumber
-	}(string(data))
-	w.WriteHeader(http.StatusAccepted)
+	w.WriteHeader(statusCode)
 	w.Write(make([]byte, 0))
 }
 
